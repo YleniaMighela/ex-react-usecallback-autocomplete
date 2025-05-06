f6133a5c615ba83e3167d49da7083ad40251e957
Ylenia <yleniamighela@gmail.com>
2025-05-06 12:20:44 +0200

    milestone 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+
 
 function App() {
 
+  const [query, setQuery] = useState("")
+  const [prodotti, setProdotti] = useState([])
+  // console.log(prodotti);
+
+
+
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/products?search=${query}`)
+      .then(res => res.json())
+      .then(data => setProdotti(data))
+      .catch(err => {
+        console.error('Errore nella chiamata API:', err)
+        setProdotti([])
+      })
+  }, [query])
+
+  console.log(prodotti);
+
   return (
     <>
-      <h1>EX - Autocomplete</h1>
+      <div>
+
+        <input type="text"
+          placeholder='Cerca il tuo prodotto..'
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+
+        />
+
+        <div className='container'>
+          {query && prodotti.length > 0 && (
+            <div >
+              {prodotti.map(product => (
+                <p className='card' key={product.id}>{product.name}</p>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </>
   )
 }
